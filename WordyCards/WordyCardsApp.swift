d61ddba484b98a370ef8588adc3fd313749dc62b
Sebastian Narvaez <sebastian.narvaez@aplicando.co>
2024-08-10 12:09:33 -0500

    Initial Commit

---
+++ WordyCards/WordyCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordyCardsApp.swift
+//  WordyCards
+//
+//  Created by Sebastian on 10/08/24.
+//
+
+import SwiftUI
+
+@main
+struct WordyCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
